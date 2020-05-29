53e99dc5d92a961eb6146bdfa412b8859d122c16
Labib Imam Chowdhury <labib.chowdhury@northsouth.edu>
2020-05-29 11:51:26 +0600

    ensemble loss added

--- speaker_id.py
+++ speaker_id.py
@@ -117,6 +117,31 @@ class ArcLoss(nn.Module):
       # print("M:", self.m)
       return log
 
+      
+class EnsembleLoss(nn.Module):
+
+  def __init__(self, margin = 0.5, s = 30):
+      super().__init__()
+
+      # self.m = nn.Parameter(torch.randn(1,1).type(torch.cuda.FloatTensor), requires_grad = True)
+      self.m = margin
+      self.s = s
+      self.epsilon = 0.0000000001
+
+  def forward(self, predicted,target):
+      
+      
+      predicted = predicted/(predicted.norm(p=2,dim = 0) + self.epsilon)
+      
+      indexes = range(predicted.size(0))
+      cos_theta_y = predicted[indexes,target]
+      theta_j = torch.acos(cos_theta_y)
+      exp_s = np.e ** (self.s * (torch.cos(theta_j*self.m + self.m)-self.m))
+      sum_cos_theta_j = (np.e ** (predicted * self.s)).sum(dim=1) - (np.e ** (predicted[indexes, target] * self.s))
+      log = -torch.log(exp_s/(exp_s + sum_cos_theta_j + self.epsilon)).mean()
+      # print("M:", self.m)
+      return log
+
 
 
 
